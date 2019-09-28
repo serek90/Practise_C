00393793f81151884445f0dfa31f6dc0bf0ca55a
serek90 <43785920+serek90@users.noreply.github.com>
2019-09-28 18:51:17 +0200

    dodanie dwóch stringów, jeden na koniec drugiego

--- Pointers5.3/main.c
+++ Pointers5.3/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
+/**
+ *Add two string second int the end of the first
+ */
 
+void strcat(char *s, char *t);
 int main()
 {
-    int a = 5, b=0, c;
-    c = a/b;
-    printf("%d", c);
-    printf("Hello world!\n");
-
+    char lancuch1[20] = "byleco";
+    char lancuch2[20] = "dodamto";
+    strcat(lancuch1, lancuch2);
+    printf("%s", lancuch1);
     return 0;
 }
+
+void strcat(char *s, char *t)
+{
+    while(*s)
+    {
+       s++;
+    }
+    while(*s++ = *t++);
+}
+
+
+
+
