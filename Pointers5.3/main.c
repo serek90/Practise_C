8d785f70fe73f36f3096c6389ce5b377dd6cb52b
serek90 <43785920+serek90@users.noreply.github.com>
2019-09-29 12:46:39 +0200

    Add new function

--- Pointers5.3/main.c
+++ Pointers5.3/main.c
@@ -6,24 +6,42 @@
  */
 
 void strcat(char *s, char *t);
+int strend(char *s, char *t);
+char strncpy (char* strTo, const char* strFrom, size_t size);
+
 int main()
 {
     char lancuch1[20] = "byleco";
     char lancuch2[20] = "dodamto";
+
     strcat(lancuch1, lancuch2);
-    printf("%s", lancuch1);
+    printf("Dodanie jednego na koniec drugiego: %s\n", lancuch1);
+
+    printf("Czy jeden na koncu drugiego? %d\n", strend(lancuch1, lancuch2));
+
+
     return 0;
 }
 
-void strcat(char *s, char *t)
+void strcat(char *s, char *t) //5.3 Dodanie jednego łańcucha na koniec drugiego
 {
     while(*s)
     {
-       s++;
+        s++;
     }
     while(*s++ = *t++);
 }
 
+int strend(char *s, char *t)  //5.4 Sprawdzenie czy drugi lancuch jest na koncu drugiego
+{
+    while(*s)
+    {
+        if(*s++ == *t)t++;
+    }
+    if(!(*t))return 1;
+    else return 0;
 
+}
 
+char strncpy(char *strTo, const char *strFrom, size_t size);  //5.5 Kopiuje okreslona liczne znakow z jednego do drugiego
 
