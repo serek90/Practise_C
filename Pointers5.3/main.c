7e17f3b1e58aadd032ef8e7ea1153ebad589f476
serek90 <43785920+serek90@users.noreply.github.com>
2019-10-02 21:08:25 +0200

    Add new function

--- Pointers5.3/main.c
+++ Pointers5.3/main.c
@@ -8,18 +8,15 @@
 void strcat(char *s, char *t);
 int strend(char *s, char *t);
 void strncpy (char* strTo, const char* strFrom, int size);// zmianic na funkcje zwracajacą char
-void strncat (char* strTo, const char* strFrom, int num);//Funkcja dopisuje pierwsze num znaków z tablicy strFrom na koniec tekstu w tablicy strTo. W przypadku gdy długość strFrom jest mniejsza niż num, funkcja przepisze wszystko.
-int strncmp(const char *s1, const char *s2, size_t n); /*Funkcja strncmp działa podobnie do funkcji strcmp, ale porównuje nie więcej niż początkowe n znaków napisów s1 i s2.
-Funkcja najpierw porównuje ze sobą pierwsze znaki napisów s1 i s2. Jeśli są równe, kontynuuje porównywanie kolejnych par aż do momentu, gdy porównane znaki będą różne, gdy w którymś napisie napotkany zostanie bajt '\0' lub gdy porównane zostanie n pierwszych znaków obu napisów.
-Zwracane wartości
-Funkcja zwraca liczbę całkowitą, która wskazuje na relację między napisami.
-Zwraca 0, gdy napisy są identyczne. Zwraca liczbę dodatnią, gdy na pierwszej pozycji, na której różnią się znaki w napisach s1 i s2, znak w s1 ma większą wartość (jako unsigned char) niż znak w s2. Zwraca liczbę ujemną w przeciwnym przypadku
-*/
+void strncat (char* strTo, const char* strFrom, int num);//Funkcja dopisuje pierwsze num znaków z tablicy strFrom na koniec tekstu w tablicy strTo.
+int strncmp(const char *s1, const char *s2, size_t n); //Porownuje n znakow z napisow, 0 gdy rowne
 
 int main()
 {
     char lancuch1[20] = "byleco";
     char lancuch2[20] = "dodamto";
+    char lanDoPor1[20] = "jakimlan";
+    char lanDoPor2[20] = "jakislap";
 
     strcat(lancuch1, lancuch2);
     printf("Dodanie jednego na koniec drugiego: %s\n", lancuch1);
@@ -28,10 +25,12 @@ int main()
 
     //strncat(lancuch1,lancuch2, 4);
     strncpy(lancuch1, lancuch2, 3);
-    printf("%s", lancuch1);
+    printf("%s\n", lancuch1);
+    printf("%d", strncmp(lanDoPor1, lanDoPor2, 5));
 
     return 0;
 }
+
 
 void strcat(char *s, char *t) //5.3 Dodanie jednego łańcucha na koniec drugiego
 {
@@ -50,8 +49,8 @@ int strend(char *s, char *t)  //5.4 Sprawdzenie czy drugi lancuch jest na koncu 
     }
     if(!(*t))return 1;
     else return 0;
-
 }
+
 
 void strncat(char *strTo, const char *strFrom, int size)  //5.5 Kopiuje okreslona liczne znakow z jednego na koniec drugiego
 {
@@ -66,6 +65,7 @@ void strncat(char *strTo, const char *strFrom, int size)  //5.5 Kopiuje okreslon
     }
 }
 
+
 void strncpy(char *strTo, const char *strFrom, int size)  //5.5 Kopiuje okreslona liczba znakow z jednoego do druiego a reszta to \0
 {
     int i = 1;
@@ -77,7 +77,19 @@ void strncpy(char *strTo, const char *strFrom, int size)  //5.5 Kopiuje okreslon
 
 }
 
-int strncmp(char *strTo, char *strTo, int size)
-{
 
+/**************************************************
+*Porównuje nie więcej niż początkowe n znaków napisów s1 i s2.
+*Funkcja zwraca liczbę całkowitą, która wskazuje na relację między napisami.
+*Zwraca 0, gdy napisy są identyczne. Zwraca liczbę dodatnią, gdy na pierwszej pozycji, na której różnią się znaki w napisach s1 i s2, znak w s1 ma większą wartość niż znak w s2.
+*Zwraca liczbę ujemną w przeciwnym przypadku
+*/
+
+int strncmp(const char *s1,const char *s2, size_t n)
+{
+    for(int i = 0; i < n; i++, s1++, s2++)
+    {
+        if(!(*s1))return 0;
+        else if(*s1 - *s2)return *s1 - *s2;
+    }
 }
