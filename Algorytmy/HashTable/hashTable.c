408bb2869a4a00a3529d2e198522c820c382584f
serek90 <jak.seroczynski@gmail.com>
2021-01-09 21:43:53 +0100

    add some changes

--- Algorytmy/HashTable/hashTable.c
+++ Algorytmy/HashTable/hashTable.c
@@ -8,6 +8,8 @@
 #define MAX_NAME 256
 #define TABLE_SIZE 10
 
+/**********************************
+*/
 typedef struct
 {
 	char name[MAX_NAME];
@@ -16,6 +18,13 @@ typedef struct
 
 }person;
 
+/**********************************/
+person *hashTable[TABLE_SIZE];
+
+/***********************************
+*hash function
+*return: place in hash table
+*********************************/
 unsigned int hash(char *name)
 {
 	int lenght = strnlen(name, MAX_NAME);
@@ -30,15 +39,78 @@ unsigned int hash(char *name)
 	return hashVal;
 }
 
+/******************************
+*Init hash table
+*return:noting
+********************************/
+
+void initHashTable(void)
+{
+	for(int i = 0; i < TABLE_SIZE; i++)
+	{
+		hashTable[i] = NULL;
+	}
+}
+
+/********************************
+*print hash table
+********************************/
+void printHashTable(void)
+{
+	printf("START HASH TABLE\n");
+	for(int i = 0; i < TABLE_SIZE; i++)
+	{
+		if(hashTable[i] == NULL)
+		{
+			printf("\t%i\t---\n", i);
+		}
+		else
+		{
+			printf("\t%i\t%s\n", i, hashTable[i]->name);
+		}
+	}
+	printf("END HASH TABLE\n");
+}
+
+/************************************
+*insert element to hash table*******
+*
+*
+************************************/
+bool hashTableInsert(person *p)
+{
+	if(p == NULL) return false;
+	int index = hash(p->name);
+	if(hashTable[index] != NULL) return false; //is not empty
+
+	hashTable[index] = p;
+	return true;
+}
+
+/************************************
+*main function
+************************************/
 int main()
 {
-	printf("Jacob => %u\n", hash("Jacob"));
+	initHashTable();
+	printHashTable();
+
+	person Jakub = {.name = "Jakub", .age = 25};
+	person Natalia = {.name = "Natalia", .age = 29};
+	person Julia = {.name = "Julia", .age = 24};
+
+	hashTableInsert(&Jakub);
+	hashTableInsert(&Natalia);
+	hashTableInsert(&Julia);
+
+	printHashTable();
+	/*printf("Jacob => %u\n", hash("Jacob"));
 	printf("Natalie => %u\n", hash("Natalie"));
 	printf("Klementyna => %u\n", hash("Klementyna"));
 	printf("Jacek => %u\n", hash("Jacek"));
 	printf("Antoni => %u\n", hash("Antoni"));
 	printf("Amelia => %u\n", hash("Amelia"));
 	printf("Julia => %u\n", hash("Julia"));
-
+	*/
 	return 0;
 }
